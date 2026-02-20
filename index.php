2eeaf563e6456c18ed9f6be056a9e25b9cc57b98
Nico <riveraent05@gmail.com>
2026-02-20 22:34:16 +0800

    changed input to button

--- index.php
+++ index.php
@@ -2,22 +2,23 @@
 <html>
 <head>
     <title>VLSM Subnet Calculator</title>
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
 </head>
 <body>
+<div class="container">
+    <h2>VLSM Calculator</h2>
 
-<h2>VLSM Calculator</h2>
+    <form action="calculate.php" method="POST">
+        Base Network (CIDR):<br>
+        <input type="text" name="base_network" placeholder="192.168.1.0/24" required>
+        <br><br>
 
-<form action="calculate.php" method="POST">
-    Base Network (CIDR):<br>
-    <input type="text" name="base_network" placeholder="192.168.1.0/24" required>
-    <br><br>
+        Host Requirements (comma separated):<br>
+        <input type="text" name="hosts" placeholder="100,50,20,10" required>
+        <br><br>
 
-    Host Requirements (comma separated):<br>
-    <input type="text" name="hosts" placeholder="100,50,20,10" required>
-    <br><br>
-
-    <input type="submit" value="Calculate">
-</form>
-
+        <button type="submit">Calculate</button>
+    </form>
+</div>
 </body>
 </html>
